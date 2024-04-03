75ae42efdb0df7b385f7eda21a0ce5c66750c1bb
sashok1987nik <sashok1987nk@ukr.net>
2024-04-03 16:55:36 +0200

    up

--- test.js
+++ test.js
@@ -1,13 +1,2 @@
-const isSuccess = true;
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve("Success! Value passed to resolve function");
-    } else {
-      reject("Error! Error passed to reject function");
-    }
-  }, 2000);
-});
-
-console.log(promise);
+const date1 = Date.now()
+console.log("date1", date1)
